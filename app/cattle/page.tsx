f4ba8d35eaea7efaf5b45df3841fdb2cb62aba1e
webdeveloper2794 <webdeveloper2794@gmail.com>
2026-06-28 08:37:59 +0300

    form dialog implementing

--- app/cattle/page.tsx
+++ app/cattle/page.tsx
@@ -1,7 +1,10 @@
+import { FormDialog } from "@/components/shared/form/form-dialog";
+
 export default function Page() {
   return (
     <div className="p-4 lg:p-6">
       <h1>Cattle List Page</h1>
+      <FormDialog type="create" />
     </div>
   );
 }
---
+++ components/shared/form/cattle-form.tsx
@@ -0,0 +1,7 @@
+export function CattleForm() {
+  return (
+    <div>
+      <h1>Cattle Form</h1>
+    </div>
+  );
+}
